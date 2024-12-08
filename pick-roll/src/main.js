50dabd7992c1cd85e999e513b95d7c8f9dbcbbf7
nicolasguaragna <fernando.guaragna@davinci.edu.ar>
2024-12-08 12:48:48 -0300

    update

--- pick-roll/src/main.js
+++ pick-roll/src/main.js
@@ -1,9 +1,9 @@
-import "./main.css"; // Importa los estilos globales
-import App from "./App.vue"; // Importa el componente principal de la app
-import { createApp } from "vue"; // Importa la función para crear la aplicación
-import router from "./router/router"; // Importa el enrutador
-import Toast from "vue-toastification"; // Importa el plugin de Toastification
-import "vue-toastification/dist/index.css"; // Importa los estilos de Toastification
+import "./main.css"; // Importo los estilos globales
+import App from "./App.vue"; // Importo el componente principal de la app
+import { createApp } from "vue"; // Importo la función para crear la aplicación
+import router from "./router/router"; // Importo el enrutador
+import Toast from "vue-toastification"; // Importo el plugin de Toastification
+import "vue-toastification/dist/index.css"; // Importo los estilos de Toastification
 
 // Configuración de opciones para Toastification
 const toastOptions = {
@@ -22,8 +22,8 @@ const toastOptions = {
 
 const app = createApp(App);
 
-// Usamos el router y el plugin de Toastification
+// Uso el router y el plugin de Toastification
 app.use(router);
 app.use(Toast, toastOptions);
 
-app.mount("#app"); // Montamos la aplicación en el contenedor de la página
+app.mount("#app"); // Monto la aplicación en el contenedor de la página
